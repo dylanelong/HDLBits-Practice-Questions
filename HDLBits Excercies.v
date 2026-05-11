f3af9045bacee065f327d68cd7704ae6c7588353
Dylan Long <dylanelong@gmail.com>
2026-05-11 15:50:00 -0400

    Add files via upload

--- HDLBits Excercies.v
+++ HDLBits Excercies.v
@@ -262,4 +262,174 @@ module top_module(input a, input b, output wire out_assign, output reg out_alway
 endmodule
 
 //Question 30: Alwaysblock2
-module 
+module top_module(input clk, input a, input b, output wire out_assign, output reg out_always_comb, output reg out_always_ff);
+    assign out_assign = a ^ b;
+    always @(*) out_always_comb = a ^ b;
+    always @(posedge clk) out_always_ff = a ^ b;
+endmodule
+
+//Question 31: Always if 
+module top_module(input a, input b, input sel_b1, input sel_b2, output out_assign, output reg out_always);
+    assign out_assign = (sel_b1&sel_b2) ? b :a;
+    always @(*) begin
+        if (sel_b1&sel_b2) begin
+            out_always = b;
+        end
+        else begin
+            out_always = a;
+        end
+    end
+endmodule
+
+//Question 32: Always if2
+module top_module (
+    input      cpu_overheated,
+    output reg shut_off_computer,
+    input      arrived,
+    input      gas_tank_empty,
+    output reg keep_driving  ); 
+    always @(*) begin
+        if (cpu_overheated)
+           shut_off_computer = 1;
+        else 
+            shut_off_computer = 0;
+    end
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+        else
+            keep_driving = 0;
+    end
+
+endmodule
+
+//Question 33: Always case
+module top_module(input[2:0] sel, input [3:0] data0, input [3:0] data1, input [3:0] data2, input [3:0] data3, input [3:0] data4, input [3:0] data5, output reg [3:0] out);
+    always @(*) begin
+        case (sel) 
+            3'b000: out = data0;
+            3'b001: out = data1;
+            3'b010: out = data2;
+            3'b011: out = data3;
+            3'b100: out = data4;
+            3'b101: out = data5;
+            default: out = 4'b0000;
+        endcase
+    end
+endmodule
+
+//Question 34: Always case2
+module top_module(input [3:0] in, output reg [1:0] pos);
+    always @(*) begin
+        if (in[0] == 1) begin
+            pos = 2'b00;
+        end
+        else if (in[1] == 1) begin
+            pos = 2'b01;
+        end 
+        else if (in[2]==1) begin
+            pos = 2'b10;
+        end
+        else if (in[3]==1) begin
+            pos = 2'b11;
+        end
+        else begin
+            pos = 2'b00;
+        end
+    end
+endmodule
+
+//Question 35: Always case 2
+module top_module(input [7:0] in, output [2:0] pos);
+    always @(*) begin
+        casez (in[7:0]) 
+            8'bzzzzzzz1: pos = 3'b000;
+            8'bzzzzzz10: pos = 3'b001;
+            8'bzzzzz100: pos = 3'b010;
+            8'bzzzz1000: pos = 3'b011;
+            8'bzzz10000: pos = 3'b100;
+            8'bzz100000: pos = 3'b101;
+            8'bz1000000: pos = 3'b110;
+            8'b10000000: pos = 3'b111;
+            default: pos = 3'b000;
+        endcase
+    end
+endmodule
+
+//Question 36: Always nolatches
+module top_module(input [15:0] scancode, output reg left, output reg down, output reg right, output reg up);
+    always @(*) begin
+        left = 1'b0;
+        right = 1'b0;
+        up = 1'b0;
+        down = 1'b0;
+        case (scancode[15:0])
+            16'he06b: left = 1'b1;
+            16'he072: down = 1'b1;
+            16'he074: right = 1'b1;
+            16'he075: up = 1'b1;
+        endcase
+    end
+endmodule
+
+//Question 37: Conditional
+module two_way_min(input [7:0] a, input [7:0] b, output reg [7:0] min);
+    always @(*) begin
+        min=a < b ? a: b;
+    end
+endmodule
+
+module top_module(input [7:0] a, input [7:0] b, input [7:0] c, input [7:0] d, output [7:0] min);
+    wire [7:0] min1;
+    wire [7:0] min2;
+    two_way_min mod1 (a,b,min1);
+    two_way_min mod2 (c,d,min2);
+    two_way_min mod3 (min1, min2, min);
+endmodule
+
+//Question 38: Reduction
+module top_module(input [7:0] in, output parity);
+    assign parity = ^in[7:0];
+endmodule
+
+//Question 39: Gates100
+module top_module (input [99:0] in, output out_and, output out_or, output out_xor);
+    assign out_and = &in[99:0];
+    assign out_or = |in[99:0];
+    assign out_xor = ^in[99:0];
+endmodule
+
+//Question 40: Vector100r
+module top_module(input [99:0] in, output reg [99:0] out);
+    always @(*) begin
+        integer in_idx;
+        integer out_idx;
+        out_idx = 0;
+        for (in_idx = 99; in_idx >= 0; in_idx = in_idx - 1) begin
+            out[out_idx] = in[in_idx];
+            out_idx = out_idx + 1;
+        end
+    end
+endmodule
+
+//Question 41: Popcount225
+module top_module(input [254:0] in, output reg [7:0] out);
+    integer idx;
+    always @(*) begin
+        out = 8'd0;
+        for (idx = 0; idx < 255; idx = idx + 1) begin
+            out = out + in[idx];
+        end
+    end
+endmodule
+
+//Question 42: Adder 100i
+module full_adder (input a, input b, input cin, output cout, output sum);
+    assign cin = (a&b)|(b&cin)|(cin&a);
+    assign sum = a^b^cin;
+endmodule
+
+module top_module(input [99:0] in, input cin, output [99:0] cout, output  [99:0] sum);
+
+
+endmodule
