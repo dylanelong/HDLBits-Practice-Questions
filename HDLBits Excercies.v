350f5a171fa662ff3073a9737328a497c30ccd0e
Dylan Long <dylanelong@gmail.com>
2026-05-11 20:51:35 -0400

    Add files via upload

--- HDLBits Excercies.v
+++ HDLBits Excercies.v
@@ -425,11 +425,180 @@ endmodule
 
 //Question 42: Adder 100i
 module full_adder (input a, input b, input cin, output cout, output sum);
-    assign cin = (a&b)|(b&cin)|(cin&a);
+    assign cout = (a&b)|(b&cin)|(cin&a);
     assign sum = a^b^cin;
 endmodule
 
-module top_module(input [99:0] in, input cin, output [99:0] cout, output  [99:0] sum);
-
-
+module top_module(input [99:0] a, b, input cin, output [99:0] cout, output  [99:0] sum);
+    genvar i;
+    generate
+        for (i = 0; i < 100; i = i+1) begin: gen_case
+            if (i == 0) begin
+                full_adder adder(a[i], b[i], cin, cout[i], sum[i]);
+            end
+            else begin
+                full_adder adder(a[i], b[i], cout[i-1], cout[i], sum[i]);
+            end
+        end
+    endgenerate
 endmodule
+
+//Question 43: Bcfadd100
+module top_module(input [399:0] a, b, input cin, output cout, output [399:0] sum);
+    wire [100:0] carry;
+    assign carry[0] = cin;   
+    assign cout = carry[100]; 
+    genvar i;
+    generate
+        for (i = 0; i < 100; i = i + 1) begin : bcd_adder_gen
+            bcd_fadd adder (.a(a[4*i +: 4]), .b(b[4*i +: 4]), .cin(carry[i]), .cout(carry[i+1]), .sum(sum[4*i +: 4]));
+        end
+    endgenerate
+endmodule
+
+//Question 44: Wire
+module top_module (
+    input in,
+    output out);
+		assign out =in;
+endmodule
+
+//Question 45: GND
+module top_module (
+    output out);
+    assign out = 1'b0;
+endmodule
+
+//Question 46: NOR
+module top_module (
+    input in1,
+    input in2,
+    output out);
+    nor(out, in1, in2);
+endmodule
+
+//Question 47: Another gate
+module top_module (
+    input in1,
+    input in2,
+    output out);
+    assign out = (~in2) & in1;
+endmodule
+
+//Question 48: Two gates
+module top_module (
+    input in1,
+    input in2,
+    input in3,
+    output out);
+    assign out = in3 ^ ~(in1^in2);
+endmodule
+
+//Question 49: Gates
+module top_module( 
+    input a, b,
+    output out_and,
+    output out_or,
+    output out_xor,
+    output out_nand,
+    output out_nor,
+    output out_xnor,
+    output out_anotb
+);
+    assign out_and = a&b;
+    assign out_or = a|b;
+    assign out_xor = a^b;
+    assign out_nand = ~(a&b);
+    assign out_nor = ~(a|b);
+    assign out_xnor = ~(a^b);
+    assign out_anotb = a & (~b);
+endmodule
+
+//Question 50: 7420
+module top_module ( 
+    input p1a, p1b, p1c, p1d,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+    assign p1y = ~(p1a&p1b&p1c&p1d);
+    assign p2y = ~(p2a&p2b&p2c&p2d);
+endmodule
+
+//Question 51: Truthtable1
+module top_module( 
+    input x3,
+    input x2,
+    input x1,  // three inputs
+    output f   // one output
+);
+    assign f = ((~x3)&x2) | (x3&x1);
+endmodule
+
+//Question 52: Mt2015 eq2
+module top_module ( input [1:0] A, input [1:0] B, output z ); 
+    assign z = A == B
+endmodule
+
+//Question 53: Mt2015 q4a
+module top_module (input x, input y, output z);
+    assign z = (x^y) & x;
+endmodule
+//Question 54: Mt2015 q4b
+module top_module ( input x, input y, output z );
+    assign z = (~x&~y) | (x&y);
+endmodule
+
+//Question 55: Mt2015 q4
+module top_module(input x, input y, output z);
+    wire z1;
+    wire z2;
+    wire z3;
+    wire z4;
+    assign z1 = (x^y) & x;
+    assign z2 = (~x&~y) | (x&y);
+    assign z3 = (x^y) & x;
+    assign z4 = (~x&~y) | (x&y);
+    assign z = ((z1|z2)|(z3&z4));
+endmodule
+
+//Question 56: Ringer
+module top_module (
+    input ring,
+    input vibrate_mode,
+    output ringer,       // Make sound
+    output motor         // Vibrate
+);
+    assign ringer = ring & ~vibrate_mode;
+    assign motor = ring & vibrate_mode;
+endmodule
+
+//Question 57: Thermostat
+module top_module (
+    input too_cold,
+    input too_hot,
+    input mode,
+    input fan_on,
+    output heater,
+    output aircon,
+    output fan
+); 
+    assign heater = (too_cold & mode);
+    assign aircon = (too_hot & ~mode);
+    assign fan = fan_on | heater |aircon;
+endmodule
+
+//Question 58: Popcount3
+module top_module( 
+    input [2:0] in,
+    output [1:0] out );
+    always @(*) begin
+        integer i;
+        out = 2'b00;
+        for (i=0;i<3;i=i+1)begin
+            out = out + (in[i] == 1);
+        end
+    end
+endmodule
+
+//Question 59: Gatesv
+
