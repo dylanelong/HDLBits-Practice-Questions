4c77eb0c42eb0e7a7787cc045292d63424ec55c4
Dylan Long <dylanelong@gmail.com>
2026-05-11 21:32:20 -0400

    Add files via upload

--- HDLBits Excercies.v
+++ HDLBits Excercies.v
@@ -601,4 +601,25 @@ module top_module(
 endmodule
 
 //Question 59: Gatesv
+module top_module( 
+    input [3:0] in,
+    output [2:0] out_both,
+    output [3:1] out_any,
+    output [3:0] out_different 
+);
+    assign out_both = in[2:0] & in[3:1];
+    assign out_any = in[3:1] | in[2:0];
+    assign out_different = in ^ {in[0], in[3:1]};
+endmodule
 
+//Question 60: Gatesv100
+module top_module( 
+    input [99:0] in,
+    output [98:0] out_both,
+    output [99:1] out_any,
+    output [99:0] out_different 
+);
+    assign out_both = in[98:0] & in[99:1];
+    assign out_any = in[99:1] | in[98:0];
+    assign out_different = in ^ {in[0], in[99:1]};
+endmodule
